e2313e13f27cbec2250943157cd85658d78a5181
NickNaso <nicoladelgobbo@gmail.com>
2016-05-23 01:07:37 +0200

    Added test suite - Added documentation - remove underscore as dependencies - fixed some bugs on MaybeMiddlewareError - changelog

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # maybe-middleware
+### Wrapper for dinamically enable or disable express middleware
+
+This wrapper allow you to dinamically enable or disable an express middleware.
+In some circumstances you would use a middleware only if some conditions are
+met.
+
+#### Installation
+If you want use maybe-middleware you have to install it. There are two methods for that:
+In your package.json add the following item:
+```json
+"maybe-middleware": "version"
+```
+then digit
+```console
+npm install
+```
+**Example**:
+```json
+"maybe-middleware": "*" for the latest version
+"maybe-middleware": "1.0.0" for the version 1.0.0
+```
+
+**OR**
+
+launch this command:
+```console
+npm install maybe-middleware --save
+```
+#### Use
+```javascript
+var maybeMiddleware = require('maybe-middleware');
+
+// Import some other required modules
+var express = require('express');
+var app = express();
+var responsePoweredBy = require('response-powered-by');
+var POWERED_BY = "@NIckNaso";
+// Some other configuration for the express app and session
+
+// predicate as boolean
+app.use(maybeMiddleware(responsePoweredBy(POWERED_BY), true));
+
+// predicate as function
+app.use(maybeMiddleware(
+  responsePoweredBy(POWERED_BY),
+  function () {
+    // some other instructions ...
+    return true;
+  }
+));
+```
+The first parameter for maybe-middleware is an express middleware, while the
+second is a boolean value or a function that return a boolean value.
+If the parameter's type do not match with those required an error will be
+dispatched to the express error middleware. In pariticular the error will be an
+instance of **MaybeMiddlewareError**.
--- test/maybe-middleware.js
+++ test/maybe-middleware.js
@@ -17,3 +17,116 @@
  ******************************************************************************/
 
 'use strict';
+
+/*!
+ * Module dependencies
+ */
+var express = require('express');
+var request = require('supertest');
+var responsePoweredBy = require('response-powered-by');
+var maybeMiddleware = require('../');
+
+var POWERED_BY = "@NickNaso";
+var DEFAULT_POWERED_BY = "Express";
+
+describe("Test maybe-middleware()", function () {
+  
+  it("Should use the responsePoweredBy middleware -- predicate is true", function (done) {
+    var app = express()
+        .use(maybeMiddleware(responsePoweredBy(POWERED_BY), true))
+        .get("/", function (req, res){
+          res.send("Maybe middleware");
+        });
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res) {
+        if(err) {
+          done.fail(err);
+        } else {
+          expect(res.get('X-Powered-By')).toEqual(POWERED_BY);
+          done();
+        }
+      });
+  });
+  
+  it("Should not use responsePoweredBy -- predicate is false", function (done) {
+    var app = express()
+        .use(maybeMiddleware(responsePoweredBy(POWERED_BY), false))
+        .get("/", function (req, res){
+          res.send("Maybe middleware");
+        });
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res) {
+        if(err) {
+          done.fail(err);
+        } else {
+          expect(res.get('X-Powered-By')).toEqual(DEFAULT_POWERED_BY);
+          done();
+        }
+      });
+    
+  });
+  
+  it("Should use responsePoweredBy -- predicate is a function that return true", function (done) {
+    var app = express()
+        .use(maybeMiddleware(responsePoweredBy(POWERED_BY), function () { return true; }))
+        .get("/", function (req, res){
+          res.send("Maybe middleware");
+        });
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res) {
+        if(err) {
+          done.fail(err);
+        } else {
+          expect(res.get('X-Powered-By')).toEqual(POWERED_BY);
+          done();
+        }
+      }); 
+  });
+  
+  it("Should not use responsePoweredBy -- predicate is a function that return false", function (done) {
+    var app = express()
+        .use(maybeMiddleware(responsePoweredBy(POWERED_BY), function () { return false; }))
+        .get("/", function (req, res){
+          res.send("Maybe middleware");
+        });
+    request(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res) {
+        if(err) {
+          done.fail(err);
+        } else {
+          expect(res.get('X-Powered-By')).toEqual(DEFAULT_POWERED_BY);
+          done();
+        }
+      }); 
+  });
+  
+  it("Should return error -- preidcate is an object", function (done) {
+    var app = express()
+        .use(maybeMiddleware(responsePoweredBy(POWERED_BY), {}))
+        .get("/", function (req, res){
+          res.send("Maybe middleware");
+        })
+        .use(function (err, req, res) {
+          res.send(500);
+        });
+    request(app)
+      .get("/")
+      .expect(500)
+      .end(function(err) {
+        if(err) {
+          done.fail(err);
+        } else {
+          done();
+        }
+      }); 
+  });
+   
+});
